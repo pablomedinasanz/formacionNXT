33f3dd92575331573804c5dfc3f0e9c4cb4bb7e4
pablomedinasanz <90444657+pablomedinasanz@users.noreply.github.com>
2022-05-19 11:36:08 +0200

    Add files via upload

---
+++ CRUD/EJ2CRUD/src/main/java/com/example/EJ2CRUD/Profesor/infrastructure/controller/dto/ProfesorOutputDTO.java
@@ -0,0 +1,30 @@
+package com.example.EJ2CRUD.Profesor.infrastructure.controller.dto;
+
+import com.example.EJ2crud.Person.domain.Person;
+import com.example.EJ2crud.Profesor.domain.Profesor;
+import com.example.EJ2crud.Student.domain.Student;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class ProfesorOutputDTO {
+
+  private String id_profesor;
+  private Person persona;
+  private List<Student> students;
+  private String branch;
+
+  public ProfesorOutputDTO(Profesor profesor) {
+    setId_profesor(profesor.getId_profesor());
+    setPersona(profesor.getPersona());
+    setStudents(profesor.getStudents());
+    setBranch(profesor.getBranch());
+  }
+}
