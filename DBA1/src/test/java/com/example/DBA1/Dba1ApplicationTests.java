7516a25ba294c87f0a6029d45e2fb316c2cc0d5a
pablomedinasanz <90444657+pablomedinasanz@users.noreply.github.com>
2022-05-23 08:37:25 +0200

    Add files via upload

---
+++ DBA1/src/test/java/com/example/DBA1/Dba1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DBA1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dba1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
